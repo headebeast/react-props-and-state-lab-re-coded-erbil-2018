282a379177437938771e673d65720d270c83fe59
Hedi Sabah <headesabah@gmail.com>
2018-09-27 07:12:06 +0000

    automatically backed up by learn

--- src/components/Pet.js
+++ src/components/Pet.js
@@ -10,6 +10,7 @@ class Pet extends React.Component {
         <div className="content">
           <a className="header">{name} {gender === 'male' ? '♂' : '♀'}</a>
           <div className="meta">
+<<<<<<< HEAD
             <span className="date">{type}</span>
           </div>
           <div className="description">
@@ -27,6 +28,21 @@ class Pet extends React.Component {
             >
               Adopt pet
             </button>
+=======
+            <span className="date">Pet type {this.props.type}</span>
+          </div>
+          <div className="description">
+            <p>Age: {this.props.age}</p>
+            <p>Weight: {this.props.weight}</p>
+          </div>
+        </div>
+        <div className="extra content">
+          {this.props.isAdopted 
+          ?  <button className="ui disabled button">Already adopted</button>
+          :<button onClick={this.props.onAdoptPet()}
+          className="ui primary button">Adopt pet</button>
+            
+>>>>>>> 9530d0f1497341cf0841f8878498ba0435341eaf
           }
         </div>
       </div>
@@ -34,7 +50,11 @@ class Pet extends React.Component {
   }
 }
 
+<<<<<<< HEAD
 export default Pet; 
+=======
+export default Pet;
+>>>>>>> 9530d0f1497341cf0841f8878498ba0435341eaf
 
 
 
